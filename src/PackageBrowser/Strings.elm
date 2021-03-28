10907e9aad7f9739946c963cc7b4478711e4f7a0
pravdomil <pravdomil.toman@gmail.com>
2021-03-28 13:44:18 +0200

    Strings

--- src/PackageBrowser/Strings.elm
+++ src/PackageBrowser/Strings.elm
@@ -1,5 +1,7 @@
 module PackageBrowser.Strings exposing (..)
 
+import Http
+
 
 title =
     "Pravdomil's Elm Packages Browser"
@@ -7,3 +9,25 @@ title =
 
 searchInput =
     "Search package or module name"
+
+
+loading =
+    "Loading..."
+
+
+httpError a =
+    case a of
+        Http.BadUrl _ ->
+            "Connection failed."
+
+        Http.Timeout ->
+            "Connection failed."
+
+        Http.NetworkError ->
+            "Connection failed."
+
+        Http.BadStatus _ ->
+            "Server seems to be broken."
+
+        Http.BadBody _ ->
+            "Server seems to be broken."
