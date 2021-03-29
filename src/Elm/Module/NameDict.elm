12aae81a4682d94f2a0dbb450b4765a758a6d1b6
pravdomil <pravdomil.toman@gmail.com>
2021-03-29 21:05:00 +0200

    Elm.Module.NameDict

---
+++ src/Elm/Module/NameDict.elm
@@ -0,0 +1,38 @@
+module Elm.Module.NameDict exposing (..)
+
+import Dict
+import Elm.Module
+
+
+type NameDict v
+    = NameDict (Dict.Dict String v)
+
+
+fromList : List ( Elm.Module.Name, v ) -> NameDict v
+fromList a =
+    a
+        |> List.map (Tuple.mapFirst Elm.Module.toString)
+        |> Dict.fromList
+        |> NameDict
+
+
+toList : NameDict v -> List ( Elm.Module.Name, v )
+toList (NameDict a) =
+    a
+        |> Dict.toList
+        |> List.filterMap (\( k, v ) -> Elm.Module.fromString k |> Maybe.map (\vv -> ( vv, v )))
+
+
+member : Elm.Module.Name -> NameDict v -> Bool
+member k (NameDict a) =
+    a |> Dict.member (Elm.Module.toString k)
+
+
+get : Elm.Module.Name -> NameDict v -> Maybe v
+get k (NameDict a) =
+    a |> Dict.get (Elm.Module.toString k)
+
+
+insert : Elm.Module.Name -> v -> NameDict v -> NameDict v
+insert k v (NameDict a) =
+    a |> Dict.insert (Elm.Module.toString k) v |> NameDict
