015328155f6a5ae272ee6e48291144844391af7a
pravdomil <pravdomil.toman@gmail.com>
2021-04-01 22:37:18 +0200

    ModuleReadme

--- src/Database/Package/Readme.elm
+++ src/Database/Package/Readme.elm
@@ -1,5 +1,6 @@
 module Database.Package.Readme exposing (..)
 
+import Dict exposing (Dict)
 import Elm.Docs
 import Elm.Module
 import Elm.Module.NameDict as PackageNameDict
@@ -7,12 +8,26 @@ import Elm.Module.NameDict as PackageNameDict
 
 type alias Readme =
     { readme : String
-    , modules : PackageNameDict.NameDict Elm.Docs.Module
+    , modules : PackageNameDict.NameDict ModuleReadme
+    }
+
+
+type alias ModuleReadme =
+    { readme : String
+    , unions : Dict String Elm.Docs.Union
+    , aliases : Dict String Elm.Docs.Alias
+    , values : Dict String Elm.Docs.Value
+    , binops : Dict String Elm.Docs.Binop
     }
 
 
 fromReadmeAndDocs : String -> List Elm.Docs.Module -> Readme
 fromReadmeAndDocs a b =
+    let
+        toDict : List { a | name : String } -> Dict String { a | name : String }
+        toDict c =
+            c |> List.map (\v -> ( v.name, v )) |> Dict.fromList
+    in
     { readme = a
     , modules =
         b
@@ -20,7 +35,17 @@ fromReadmeAndDocs a b =
                 (\v ->
                     v.name
                         |> Elm.Module.fromString
-                        |> Maybe.map (\vv -> ( vv, v ))
+                        |> Maybe.map
+                            (\vv ->
+                                ( vv
+                                , { readme = v.comment
+                                  , unions = v.unions |> toDict
+                                  , aliases = v.aliases |> toDict
+                                  , values = v.values |> toDict
+                                  , binops = v.binops |> toDict
+                                  }
+                                )
+                            )
                 )
             |> PackageNameDict.fromList
     }
