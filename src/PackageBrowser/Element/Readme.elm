6b2bbbde56e3c66ab70437b792377a61ed6a8c8a
pravdomil <pravdomil.toman@gmail.com>
2021-04-03 12:33:57 +0200

    reorder

--- src/PackageBrowser/Element/Readme.elm
+++ src/PackageBrowser/Element/Readme.elm
@@ -318,58 +318,6 @@ viewModuleReadme a b expanded c =
     viewReadme view_ c
 
 
-replaceDocs : String -> String
-replaceDocs a =
-    let
-        regex : Regex.Regex
-        regex =
-            "\n@docs (.*)"
-                |> Regex.fromString
-                |> Maybe.withDefault Regex.never
-
-        escape : String -> String
-        escape b =
-            b
-                |> String.replace "&" "&amp;"
-                |> String.replace "'" "&apos;"
-                |> String.replace "\"" "&quot;"
-                |> String.replace "<" "&lt;"
-                |> String.replace ">" "&gt;"
-    in
-    a
-        |> Regex.replace regex
-            (\v ->
-                v.submatches
-                    |> List.head
-                    |> Maybe.andThen identity
-                    |> Maybe.withDefault ""
-                    |> (\vv -> "\n<docs value=\"" ++ escape vv ++ "\"></docs>")
-            )
-
-
-blocksToSections : String -> List Markdown.Block.Block -> List ( String, List Markdown.Block.Block )
-blocksToSections defaultTitle a =
-    let
-        fold : Markdown.Block.Block -> List ( String, List Markdown.Block.Block ) -> List ( String, List Markdown.Block.Block )
-        fold b acc =
-            case b of
-                Markdown.Block.Heading _ c ->
-                    ( Markdown.Block.extractInlineText c, [] ) :: acc
-
-                _ ->
-                    case acc of
-                        [] ->
-                            ( defaultTitle, [ b ] ) :: acc
-
-                        ( title, c ) :: rest ->
-                            ( title, b :: c ) :: rest
-    in
-    a
-        |> List.foldl fold []
-        |> List.map (Tuple.mapSecond List.reverse)
-        |> List.reverse
-
-
 viewBlocks : Readme.ModuleReadme -> Bool -> List Markdown.Block.Block -> Element msg
 viewBlocks module_ expand a =
     let
@@ -594,6 +542,62 @@ typeToString a =
             ]
                 |> String.join " "
                 |> List.singleton
+
+
+
+--
+
+
+replaceDocs : String -> String
+replaceDocs a =
+    let
+        regex : Regex.Regex
+        regex =
+            "\n@docs (.*)"
+                |> Regex.fromString
+                |> Maybe.withDefault Regex.never
+
+        escape : String -> String
+        escape b =
+            b
+                |> String.replace "&" "&amp;"
+                |> String.replace "'" "&apos;"
+                |> String.replace "\"" "&quot;"
+                |> String.replace "<" "&lt;"
+                |> String.replace ">" "&gt;"
+    in
+    a
+        |> Regex.replace regex
+            (\v ->
+                v.submatches
+                    |> List.head
+                    |> Maybe.andThen identity
+                    |> Maybe.withDefault ""
+                    |> (\vv -> "\n<docs value=\"" ++ escape vv ++ "\"></docs>")
+            )
+
+
+blocksToSections : String -> List Markdown.Block.Block -> List ( String, List Markdown.Block.Block )
+blocksToSections defaultTitle a =
+    let
+        fold : Markdown.Block.Block -> List ( String, List Markdown.Block.Block ) -> List ( String, List Markdown.Block.Block )
+        fold b acc =
+            case b of
+                Markdown.Block.Heading _ c ->
+                    ( Markdown.Block.extractInlineText c, [] ) :: acc
+
+                _ ->
+                    case acc of
+                        [] ->
+                            ( defaultTitle, [ b ] ) :: acc
+
+                        ( title, c ) :: rest ->
+                            ( title, b :: c ) :: rest
+    in
+    a
+        |> List.foldl fold []
+        |> List.map (Tuple.mapSecond List.reverse)
+        |> List.reverse
 
 
 
