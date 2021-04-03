aea6960abe8b5cb184231cfee83996026d3edd73
pravdomil <pravdomil.toman@gmail.com>
2021-04-03 14:50:10 +0200

    PackageBrowser.Ui.Alias

---
+++ src/PackageBrowser/Ui/Alias.elm
@@ -0,0 +1,23 @@
+module PackageBrowser.Ui.Alias exposing (..)
+
+import Element
+
+
+type alias Element msg =
+    Element.Element msg
+
+
+none =
+    Element.none
+
+
+text =
+    Element.text
+
+
+el =
+    Element.el
+
+
+
+--
