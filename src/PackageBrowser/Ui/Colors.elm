a28b120bb7c1038237ca48439b7fda5c53211ccb
pravdomil <pravdomil.toman@gmail.com>
2021-04-05 13:44:48 +0200

    color utils

--- src/PackageBrowser/Ui/Colors.elm
+++ src/PackageBrowser/Ui/Colors.elm
@@ -1,6 +1,9 @@
 module PackageBrowser.Ui.Colors exposing (..)
 
 import Element
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
 
 
 gray0 =
@@ -73,3 +76,219 @@ warning =
 
 danger =
     Element.rgb 0.86 0.21 0.27
+
+
+
+-- BACKGROUND COLOR
+
+
+bgGray0 =
+    Background.color gray0
+
+
+bgGray1 =
+    Background.color gray1
+
+
+bgGray2 =
+    Background.color gray2
+
+
+bgGray3 =
+    Background.color gray3
+
+
+bgGray4 =
+    Background.color gray4
+
+
+bgGray5 =
+    Background.color gray5
+
+
+bgGray6 =
+    Background.color gray6
+
+
+bgGray7 =
+    Background.color gray7
+
+
+bgGray8 =
+    Background.color gray8
+
+
+bgGray9 =
+    Background.color gray9
+
+
+bgGray10 =
+    Background.color gray10
+
+
+bgPrimary =
+    Background.color primary
+
+
+bgSecondary =
+    Background.color secondary
+
+
+bgSuccess =
+    Background.color success
+
+
+bgInfo =
+    Background.color info
+
+
+bgWarning =
+    Background.color warning
+
+
+bgDanger =
+    Background.color danger
+
+
+
+-- FONT COLOR
+
+
+fontGray0 =
+    Font.color gray0
+
+
+fontGray1 =
+    Font.color gray1
+
+
+fontGray2 =
+    Font.color gray2
+
+
+fontGray3 =
+    Font.color gray3
+
+
+fontGray4 =
+    Font.color gray4
+
+
+fontGray5 =
+    Font.color gray5
+
+
+fontGray6 =
+    Font.color gray6
+
+
+fontGray7 =
+    Font.color gray7
+
+
+fontGray8 =
+    Font.color gray8
+
+
+fontGray9 =
+    Font.color gray9
+
+
+fontGray10 =
+    Font.color gray10
+
+
+fontPrimary =
+    Font.color primary
+
+
+fontSecondary =
+    Font.color secondary
+
+
+fontSuccess =
+    Font.color success
+
+
+fontInfo =
+    Font.color info
+
+
+fontWarning =
+    Font.color warning
+
+
+fontDanger =
+    Font.color danger
+
+
+
+-- BORDER COLOR
+
+
+borderGray0 =
+    Border.color gray0
+
+
+borderGray1 =
+    Border.color gray1
+
+
+borderGray2 =
+    Border.color gray2
+
+
+borderGray3 =
+    Border.color gray3
+
+
+borderGray4 =
+    Border.color gray4
+
+
+borderGray5 =
+    Border.color gray5
+
+
+borderGray6 =
+    Border.color gray6
+
+
+borderGray7 =
+    Border.color gray7
+
+
+borderGray8 =
+    Border.color gray8
+
+
+borderGray9 =
+    Border.color gray9
+
+
+borderGray10 =
+    Border.color gray10
+
+
+borderPrimary =
+    Border.color primary
+
+
+borderSecondary =
+    Border.color secondary
+
+
+borderSuccess =
+    Border.color success
+
+
+borderInfo =
+    Border.color info
+
+
+borderWarning =
+    Border.color warning
+
+
+borderDanger =
+    Border.color danger
