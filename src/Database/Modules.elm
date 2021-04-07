10744876f79299dde8f705864ef2a6f603935cc5
pravdomil <pravdomil.toman@gmail.com>
2021-04-07 21:23:17 +0200

    Database.Modules

---
+++ src/Database/Modules.elm
@@ -0,0 +1,59 @@
+module Database.Modules exposing (..)
+
+import Database.Package as Package
+import Elm.Module as Module
+import Elm.Module.Encode
+import Elm.Module.NameDict as NameDict
+import Elm.Package as Package
+import Elm.Package.Encode
+import Json.Encode as Encode
+import Utils.Json.Encode_ as Encode_
+
+
+type alias Modules =
+    List ( Module.Name, List ( Package.Name, Module.Name ) )
+
+
+type alias ModuleDict =
+    NameDict.NameDict (List ( Package.Name, Module.Name ))
+
+
+fromPackages : List Package.Package -> Modules
+fromPackages a =
+    let
+        fold : ( Package.Name, Module.Name ) -> ModuleDict -> ModuleDict
+        fold ( package, module_ ) acc =
+            let
+                key : Module.Name
+                key =
+                    if package |> Package.toString |> String.contains "/elm-review" then
+                        Module.fromString ("Review.Rule." ++ Module.toString module_)
+                            |> Maybe.withDefault module_
+
+                    else
+                        module_
+            in
+            acc
+                |> NameDict.update
+                    key
+                    (\v ->
+                        v |> Maybe.withDefault [] |> (::) ( package, module_ ) |> Just
+                    )
+    in
+    a
+        |> List.concatMap
+            (\v ->
+                v.exposed |> Package.exposedToList |> List.map (Tuple.pair v.name)
+            )
+        |> List.foldl fold NameDict.empty
+        |> NameDict.toList
+        |> List.map (Tuple.mapSecond List.reverse)
+        |> List.sortBy (Tuple.first >> Module.toString >> String.toLower)
+
+
+encode : Modules -> Encode.Value
+encode =
+    Encode.list
+        (Encode_.tuple Elm.Module.Encode.name
+            (Encode.list (Encode_.tuple Elm.Package.Encode.name Elm.Module.Encode.name))
+        )
