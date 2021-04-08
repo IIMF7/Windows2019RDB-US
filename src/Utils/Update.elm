9903232e15e18a592891f8ec2dd5e134b874a789
pravdomil <pravdomil.toman@gmail.com>
2021-04-08 13:53:56 +0200

    Update.multiple

--- src/Utils/Update.elm
+++ src/Utils/Update.elm
@@ -8,3 +8,19 @@ More information: <https://sporto.github.io/elm-patterns/architecture/update-ret
 andThen : (model -> ( model, Cmd msg )) -> ( model, Cmd msg ) -> ( model, Cmd msg )
 andThen fn ( model, cmd ) =
     fn model |> Tuple.mapSecond (\v -> Cmd.batch [ v, cmd ])
+
+
+multiple : List msg -> (msg -> model -> ( model, Cmd msg )) -> model -> ( model, Cmd msg )
+multiple messages fn initialModal =
+    let
+        fold : msg -> ( model, Cmd msg ) -> ( model, Cmd msg )
+        fold msg ( model, cmd ) =
+            let
+                ( nextModel, nextMsg ) =
+                    fn msg model
+            in
+            ( nextModel
+            , Cmd.batch [ nextMsg, cmd ]
+            )
+    in
+    messages |> List.foldl fold ( initialModal, Cmd.none )
